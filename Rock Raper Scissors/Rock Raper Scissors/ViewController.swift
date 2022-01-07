b4489759c9edc1b45cd426b6e217d6bc8937fbb7
feras.saud10@gmail.com <feras.saud10@gmail.com>
2022-01-07 07:27:52 +0300

    link stoyboard with code

--- Rock Raper Scissors/Rock Raper Scissors/ViewController.swift
+++ Rock Raper Scissors/Rock Raper Scissors/ViewController.swift
@@ -12,14 +12,30 @@ class ViewController: UIViewController {
     @IBOutlet weak var UiViewBackground: UIView!
     @IBOutlet weak var startButton: UIButton!
     
+    @IBOutlet weak var FirstPlayerImage: UIImageView!
+    @IBOutlet weak var SecandPlayerImage: UIImageView!
+    
+    // Array of image
+    let ArrayOfImage = [#imageLiteral(resourceName: "scissors"), #imageLiteral(resourceName: "hand"), #imageLiteral(resourceName: "Rock")]
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       
         
+        // Add rounded borders to an UIView and UIButton
         UiViewBackground.layer.cornerRadius = 15
         startButton.layer.cornerRadius = 15
+        
     }
 
-
+    // To get result
+    @IBAction func startPressedButton(_ sender: UIButton) {
+        print("Start")
+        
+        FirstPlayerImage.image = ArrayOfImage[Int.random(in: 0...2)]
+        
+        print(ArrayOfImage.enumerated())
+              
+    }
 }
 
