23713d24ce54134cbd2e05d8fc8dc528064989ef
FAlmutairi <feras.saud10@gmail.com>
2022-05-01 05:47:07 +0300

    add new storyboard

---
+++ Rock Raper Scissors/Rock Raper Scissors/RegistrationVC.swift
@@ -0,0 +1,103 @@
+//
+//  RegistrationVC.swift
+//  Rock Raper Scissors
+//
+//  Created by FERAS on 28/04/2022.
+//
+
+import UIKit
+
+class RegistrationVC: UIViewController {
+
+    @IBOutlet weak var RegistrationView: UIView!
+    @IBOutlet weak var playerOne: UITextField!
+    @IBOutlet weak var playerTwo: UITextField!
+    
+    @IBOutlet weak var TexFieldFirstPlayer: UITextField!
+    @IBOutlet weak var TextFieldSecondPlayr: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+//        RegistrationView.layer.cornerRadius = 15
+//        RegistrationView.layer.shadowColor = UIColor.black.cgColor
+//        RegistrationView.layer.shadowOpacity = 0.2
+//        RegistrationView.layer.shadowOffset = .zero
+//        RegistrationView.layer.shadowRadius = 4
+        
+        TexFieldFirstPlayer.delegate = self
+        TextFieldSecondPlayr.delegate = self
+    }
+    
+    @IBAction func startPressed(_ sender: UIButton) {
+//
+//        let FirstPlayer = playerOne.text
+////
+//        if FirstPlayer == "" {
+//            print(" pleas Entet name")
+//            TexFieldFirstPlayer.backgroundColor = UIColor(red: 255/255.0, green: 0/255.0, blue: 0/255.0, alpha: 0.4)
+//            TextFieldSecondPlayr.backgroundColor = UIColor(red: 255/255.0, green: 0/255.0, blue: 0/255.0, alpha: 1)
+//
+//            TexFieldFirstPlayer.placeholder = "إدخل الاسم من فضلك!"
+//            TextFieldSecondPlayr.placeholder = "إدخل الاسم من فضلك!"
+//        }
+//
+        TextFieldSecondPlayr.endEditing(true)
+        TexFieldFirstPlayer.endEditing(true)
+        
+        
+        
+    }
+    
+    
+}
+
+//MARK: - UITextFieldDelehste
+
+extension RegistrationVC: UITextFieldDelegate{
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        print(TexFieldFirstPlayer.text!)
+        print(TextFieldSecondPlayr.text!)
+        TextFieldSecondPlayr.endEditing(true)
+        TexFieldFirstPlayer.endEditing(true)
+        return true
+    }
+    
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+
+        if TexFieldFirstPlayer.text == ""{
+            TexFieldFirstPlayer.backgroundColor = UIColor(red: 255/255.0, green: 0/255.0, blue: 0/255.0, alpha: 1)
+            TexFieldFirstPlayer.placeholder = "إدخل الاسم من فضلك!"
+            print("please enter you name")
+            return true
+            
+        } else if TextFieldSecondPlayr.text == "" {
+            TextFieldSecondPlayr.backgroundColor = UIColor(red: 255/255.0, green: 0/255.0, blue: 0/255.0, alpha: 1)
+            TextFieldSecondPlayr.placeholder = "إدخل الاسم من فضلك!"
+            
+            return false
+        }else{
+            return false
+        }
+        
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField){
+        
+        if TexFieldFirstPlayer.text != "" && TextFieldSecondPlayr.text != "" {
+            let gameVC = storyboard?.instantiateViewController(withIdentifier: "ViewController") as! ViewController
+            gameVC.FName = TexFieldFirstPlayer.text!
+            gameVC.SName = TextFieldSecondPlayr.text!
+            present(gameVC, animated: true, completion: nil)
+            TexFieldFirstPlayer.text = ""
+            TextFieldSecondPlayr.text = ""
+            
+        }
+        
+    }
+    
+}
--- Rock Raper Scissors/Rock Raper Scissors/ViewController.swift
+++ Rock Raper Scissors/Rock Raper Scissors/ViewController.swift
@@ -15,6 +15,12 @@ class ViewController: UIViewController {
     @IBOutlet weak var FirstPlayerImage: UIImageView!
     @IBOutlet weak var SecandPlayerImage: UIImageView!
     
+    @IBOutlet weak var firstName: UILabel!
+    @IBOutlet weak var secandName: UILabel!
+    
+    var FName: String = ""
+    var SName: String = ""
+    
     // Array of image
     let ArrayOfImage = [#imageLiteral(resourceName: "scissors"), #imageLiteral(resourceName: "hand"), #imageLiteral(resourceName: "Rock")]
 
@@ -25,6 +31,8 @@ class ViewController: UIViewController {
         // Add rounded borders to an UIView and UIButton
         UiViewBackground.layer.cornerRadius = 15
         startButton.layer.cornerRadius = 15
+        firstName.text = "\(FName)"
+        secandName.text = "\(SName)"
         
     }
 
@@ -33,8 +41,16 @@ class ViewController: UIViewController {
         print("Start")
         
         FirstPlayerImage.image = ArrayOfImage[Int.random(in: 0...2)]
+        SecandPlayerImage.image = ArrayOfImage[Int.random(in: 0...2)]
         
-        print(ArrayOfImage.enumerated())
+        if SecandPlayerImage.image == FirstPlayerImage.image {
+            print("True")
+        } else {
+            print("False")
+        }
+        
+        
+  
               
     }
 }
